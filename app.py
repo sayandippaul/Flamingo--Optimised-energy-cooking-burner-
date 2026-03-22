467c9af1475f65d63a3e1e90c2487293f3ea1c1f
sayandippaul <sayandip126@gmail.com>
2026-03-22 14:06:01 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -296,7 +296,8 @@ def arduino_none():
 
 # ================================ RUN SERVER ============================
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
